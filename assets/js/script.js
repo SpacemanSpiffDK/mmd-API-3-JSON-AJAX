dd952f9a8d5a218c4c00188b028f19a18634a362
Dan Høegh <dan.hoegh@gmail.com>
2020-03-17 11:29:04 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@
 // Wiki URL
 // https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=
 
-const showItemAmount = 5; // global variable!
+let showItemAmount = 5; // global variable!
 
 initSearchTool(); // call initSearchTool()
 
